4f5f5d4f19711d5afe8c2e6d695f1afad491eb89
Hassan Shahzad <xhah450@gmail.com>
2023-09-27 00:23:41 +0500

    first commit

--- src/controllers/employee.ts
+++ src/controllers/employee.ts
@@ -6,17 +6,20 @@ import express from 'express';
 import authenticateToken from '../middleware/auth';
 
 const router = express.Router();
+// router.post('/new', authenticateToken ,async (req: express.Request, res: express.Response) => {
 
-router.post('/new', authenticateToken,async (req: express.Request, res: express.Response) => {
+router.post('/new', async (req: express.Request, res: express.Response) => {
     try {
         const contactNo = await Employee.findOne({ employeeId: req.body.employeeId });
-        if (contactNo) {
-            return res.status(400).json({ message: "Employee already exists" });
-        } else {
-            await Employee.create(req.body);
-            res.status(200).json({ message: "Employee Added Sucessfully" });
+        await Employee.create(req.body);
+        res.status(200).json({ message: "Employee Added Sucessfully" });
+        // if (contactNo) {
+        //     return res.status(400).json({ message: "Employee already exists" });
+        // } else {
+        //     await Employee.create(req.body);
+        //     res.status(200).json({ message: "Employee Added Sucessfully" });
 
-        }
+        // }
     } catch (error) {
         res.status(400).json({ message: error.message });
     }
